c7c502dd91aa70aca3638133bb880c2376b3f5d6
ojinwayo <ojinwayo@gmail.com>
2026-07-26 09:12:59 +0100

    Create favorite_numbers.py
    Complete Exercise 6-2 Favorite Numbers

---
+++ chapter_06/favorite_numbers.py
@@ -0,0 +1,26 @@
+#Project: Dictionary - (PCC Exercis 6-2)
+#Author: Paulson Idoko
+#Date: 26th July, 2026
+
+#--- Phase - 1 Favorite Numbers ---
+
+"""
+Task 6-2:Favorite Numbers.
+Use a dictionary to store people’s favorite numbers.
+Think of five names, and use them as keys in your dictionary. Think of a favorite 
+number for each person, and store each as a value in your dictionary. Print 
+each person’s name and their favorite number. For even more fun, poll a few 
+friends and get some actual data for your program.
+"""
+favorite_numbers = {
+	'steve': 12,
+	'juliet': 17,
+	'messi': 20,
+	'tyson': 25,
+	'emeka': 90,
+}
+print(f"Steve's favorite number is {favorite_numbers['steve']}.")
+print(f"Juliet's favorite number is {favorite_numbers['juliet']}.")
+print(f"Messi's favorite number is {favorite_numbers['messi']}.")
+print(f"Tyson's favorite number is {favorite_numbers['tyson']}.")
+print(f"Emeka's favorite number is {favorite_numbers['emeka']}.")
